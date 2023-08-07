4df99223d35e63a29784d63d7c51699869b05061
Mohammad Alavi <43592603+mohammad00alavi@users.noreply.github.com>
2023-08-07 17:43:50 +0100

    modal

---
+++ src/components/layout/Modal/Demo.tsx
@@ -0,0 +1,15 @@
+import Modal from "./Modal";
+
+const ModalDemo = () => {
+    return (
+        <Modal>
+            <h3>Lorem, ipsum dolor.</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias,
+                ea.
+            </p>
+        </Modal>
+    );
+};
+
+export default ModalDemo;
