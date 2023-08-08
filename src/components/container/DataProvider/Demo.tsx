18193b91ed273aadf6a3f9572ceccda8e81d9eab
Mohammad Alavi <43592603+mohammad00alavi@users.noreply.github.com>
2023-08-08 14:46:57 +0100

    data-provider-2

---
+++ src/components/container/DataProvider/Demo.tsx
@@ -0,0 +1,24 @@
+import DataProvider from "./version-2/DataProvider";
+
+interface Props {
+    heading?: string;
+}
+
+const getLocalStorageData = (key: string) => () => {
+    return localStorage.getItem(key) || "No data found";
+};
+
+const Heading = ({ heading }: Props) => <h1>{heading}</h1>;
+
+const DataProviderDemo = () => {
+    return (
+        <DataProvider
+            getDataFunc={getLocalStorageData("heading")}
+            dataKey="heading"
+        >
+            <Heading />
+        </DataProvider>
+    );
+};
+
+export default DataProviderDemo;
