bc37f5eb30aa2a22d0330f2cdb11fd668c85bacf
Mohammad Alavi <43592603+mohammad00alavi@users.noreply.github.com>
2023-08-10 16:50:18 +0100

    recursive-list

---
+++ src/components/functional-patterns/RecursiveList/Demo.tsx
@@ -0,0 +1,8 @@
+import RecursiveList from "./RecursiveList";
+import { data } from "./utils";
+
+const RecursiveListDemo = () => {
+    return <RecursiveList data={data} />;
+};
+
+export default RecursiveListDemo;
